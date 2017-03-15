58fe2f3b82aa8dbbc0317d97721252db6d06c517
jayant-yadav <jayantyadav202@gmail.com>
2017-03-15 19:19:07 +0530

    API in progress

--- modules/actionValidate.go
+++ modules/actionValidate.go
@@ -12,9 +12,25 @@ import (
 	"mig.ninja/mig"
 	"mig.ninja/mig/pgp"
 )
-// This API receives the "operation" parameter and marshalls it the way actin/create API wants it and POSTs it 
+// This API receives the "operation" parameter and marshalls it the way actin/create API wants it and POSTs it
 func actionValidate(respWriter http.ResponseWriter, request *http.Request) {
 
+	err = request.ParseForm()
+	if err != nil {
+		panic(err)
+	}
 
+postAction := request.FormValue("parameters")
+var parameters map[string]interface{}
+if err := json.Unmarshal(byt, &parameters); err != nil {
+        panic(err)
+    }
+    fmt.Println(parameters)
+
+		postAction := request.FormValue("parameters")
+		err = json.Unmarshal([]byte(postAction), &parameters)
+		if err != nil {
+			panic(err)
+		}
 
 }
