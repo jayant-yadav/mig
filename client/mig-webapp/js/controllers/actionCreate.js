16a4f3f424e0488c33c16ac182ddbaf8200f6553
jayant yadav <jayantyadav202@gmail.com>
2017-04-20 23:13:44 +0530

    http post added to test json

--- client/mig-webapp/js/controllers/actionCreate.js
+++ client/mig-webapp/js/controllers/actionCreate.js
@@ -11,29 +11,60 @@ app.controller('actionCreateCtrl', ['$scope', '$q', '$mdDialog', '$mdSidenav', '
     }
 
 
-/*
-    $scope.operation[] = {
-        module: "",
-        parameters: {
-            description: "",
-            paths: "",
-            contents: "",
-            names: "",
-            sizes: "",
-            modes: "",
-            mtimes: "",
-            md5: "",
-            sha1: "",
-            sha2: "",
-            sha3: "",
-            options: {
-                maxdepth: "",
-                maxerrors: "",
-                fill rest from https: //github.com/mozilla/mig/blob/master/modules/file/file.go#L95
-            }
-        }
-    };*/
+    /*
+        $scope.operation[] = {
+            module: "",
+            parameters: {
+                description: "",
+                paths: "",
+                contents: "",
+                names: "",
+                sizes: "",
+                modes: "",
+                mtimes: "",
+                md5: "",
+                sha1: "",
+                sha2: "",
+                sha3: "",
+                options: {
+                    maxdepth: "",
+                    maxerrors: "",
+                    fill rest from https: //github.com/mozilla/mig/blob/master/modules/file/file.go#L95
+                }
+            } 
+        };*/
 
     /*CAll the action validate api here*/
+
+   /* $http({
+        method: 'POST',
+        url: '../api/v1/action/actionSerialize/',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        transformRequest: function (obj) {
+            var str = [];
+            for (var p in obj)
+                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            return str.join("&");
+        },
+        data: {
+            //{
+                age: 24,
+                id: "k34rAT4"
+
+            //}
+        }
+    })*/
+    $http.post('../api/v1/action/actionSerialize/',{id: 24,age: "k34rAT4"}).success(function (response) {
+        console.log(response);
+        /* $mdToast.show($mdToast.simple().textContent('Investigator Updated!').position('right').hideDelay(3000));
+         $mdDialog.hide();*/
+    }).error(function (err) {
+        //        window.alert(err.collection.error.message);
+        console.log(err);
+    });
+
+
 
 }]);
